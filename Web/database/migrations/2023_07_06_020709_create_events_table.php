69826756793a3a204c0bf0a47d00c7e5c5950b63
Mustleaf <sayid05ffgo@gmail.com>
2023-07-06 10:38:15 +0700

    database

---
+++ Web/database/migrations/2023_07_06_020709_create_events_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('eventid');
+            // $table->integer('userid');
+            $table->string('uraian');
+            $table->string('tujuan');
+            $table->date('tanggal');
+            $table->time('waktu');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
--- Web/tailwind.config.js
+++ Web/tailwind.config.js
@@ -8,7 +8,6 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.jsx',
-        "./src/**/*.{html,js}"
     ],
 
     theme: {
