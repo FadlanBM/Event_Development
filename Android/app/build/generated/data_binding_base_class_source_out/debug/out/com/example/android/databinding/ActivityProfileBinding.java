dc4427690294d963a2af0ea274753d7c2357725b
FadlanBM <104897512+FadlanBM@users.noreply.github.com>
2023-07-10 07:00:32 +0700

    design profile

--- Android/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/android/databinding/ActivityProfileBinding.java
+++ Android/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/android/databinding/ActivityProfileBinding.java
@@ -4,25 +4,33 @@ package com.example.android.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.viewbinding.ViewBinding;
+import androidx.viewbinding.ViewBindings;
 import com.example.android.R;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class ActivityProfileBinding implements ViewBinding {
   @NonNull
-  private final ConstraintLayout rootView;
+  private final LinearLayout rootView;
 
-  private ActivityProfileBinding(@NonNull ConstraintLayout rootView) {
+  @NonNull
+  public final ImageView btnBackProfile;
+
+  private ActivityProfileBinding(@NonNull LinearLayout rootView,
+      @NonNull ImageView btnBackProfile) {
     this.rootView = rootView;
+    this.btnBackProfile = btnBackProfile;
   }
 
   @Override
   @NonNull
-  public ConstraintLayout getRoot() {
+  public LinearLayout getRoot() {
     return rootView;
   }
 
@@ -43,10 +51,19 @@ public final class ActivityProfileBinding implements ViewBinding {
 
   @NonNull
   public static ActivityProfileBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.btn_back_profile;
+      ImageView btnBackProfile = ViewBindings.findChildViewById(rootView, id);
+      if (btnBackProfile == null) {
+        break missingId;
+      }
 
-    return new ActivityProfileBinding((ConstraintLayout) rootView);
+      return new ActivityProfileBinding((LinearLayout) rootView, btnBackProfile);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
