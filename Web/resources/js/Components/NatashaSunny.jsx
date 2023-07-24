363fb473f51a43717502b646e83a23817ae0d9ae
r44ph <revaaph15@gmail.com>
2023-07-24 13:47:55 +0700

    7/24/23 Frontend ( Home )

---
+++ Web/resources/js/Components/NatashaSunny.jsx
@@ -0,0 +1,9 @@
+export default function NatashaSunny() {
+    return (
+        <div className="bg-[url('../js/assets/natashasunny.png')] h-[500px] w-[500px]">
+            <div className="">
+
+            </div>
+        </div>
+    );
+}
--- Web/resources/js/Pages/Home.jsx
+++ Web/resources/js/Pages/Home.jsx
@@ -1,33 +1,69 @@
 import { Link, router } from "@inertiajs/react";
 import Ornament from "../assets/Ornament.svg";
 import home from "../assets/home.png";
+import Assets from "../assets/Assets.png";
+import NatashaSunny from "@/Components/NatashaSunny";
 
 export default function Home() {
     return (
-        <div className="flex py-[7rem] px-[8rem] xl:px-[14rem]">
-            <div>
-                <h1 className="xl:text-[6rem] text-[4rem] leading-[6rem] font-semibold">
-                    Reliable, secure
-                    <img className="inline align-top" src={Ornament} />
-                    <br />
-                    conference to get
-                    <br />
-                    the best events
-                </h1>
-                <p className="text-[#57585D] mt-[2rem] text-[20px]">
-                    Hold incredible events, share knowledge, build and grow
-                    your,
-                    <br />
-                    product , create opportunity
-                </p>
-                <Link href="/join">
-                    <button className="w-[198px] h-[50px] mt-9 text-white font-medium rounded-md bg-[#E14817]">Join A Meeting</button>
-                </Link>
+        <>
+            <div className="flex py-[7rem] px-[5rem] xl:px-[14rem] bg-[#0F1017]">
+                <div className="pt-[3rem]">
+                    <h1 className="xl:text-[6rem] text-[4rem] leading-[4.7rem] text-white font-semibold">
+                        Reliable, secure
+                        <img className="inline align-top" src={Ornament} />
+                        <br />
+                        conference to get
+                        <br />
+                        the best events
+                    </h1>
+                    <p className="text-[#CFCFD1] mt-[2rem] text-[17px]">
+                        Hold incredible events, share knowledge, build and grow
+                        your
+                        <br />
+                        product , create opportunity
+                    </p>
+                    <Link href="/join">
+                        <button className="w-[198px] h-[50px] mt-9 text-white font-medium rounded-md bg-[#E14817]">
+                            Join A Meeting
+                        </button>
+                    </Link>
+                </div>
+
+                <div>
+                    <img className="ml-[4.5rem]" src={home} />
+                </div>
             </div>
 
-            <div>
-                <img className="ml-[2.6rem]" src={home} />
+            <div className="bg-[#F8F8F8] h-[589px] flex justify-center items-center">
+                <div className="flex items-center gap-x-[3.8rem]">
+                    <div>
+                        <img src={Assets} />
+                    </div>
+
+                    <div>
+                        <h1 className="text-[2.7rem] text-black font-semibold leading-[58px]">
+                            Everyone on their
+                            <br />
+                            own device.
+                        </h1>
+                        <p className="text-[#57585D] mt-[15px] leading-8 tracking-wider text-[17px]">
+                            <span className="font-semibold">EventUp</span> is
+                            designed to be inclusive, by being
+                            <br />
+                            able to use your own device to help hybrid-
+                            <br />
+                            conference teams create, collaborate and
+                            <br />
+                            celebrate together.
+                        </p>
+                    </div>
+                </div>
             </div>
-        </div>
+
+            <div className="bg-white h-[1066px]">
+                <NatashaSunny />
+            </div>
+        </>
     );
 }
