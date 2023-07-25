b071b124e6ae90d5f4557e6bc1612ce06f7bf36b
r44ph <revaaph15@gmail.com>
2023-07-25 09:20:36 +0700

    h

--- Web/resources/js/Components/NatashaSunny.jsx
+++ Web/resources/js/Components/NatashaSunny.jsx
@@ -5,7 +5,7 @@ export default function NatashaSunny() {
     return (
         <div className="bg-[url('../js/assets/natashasunny.png')] h-[500px] w-[500px] flex">
             <div className="flex">
-                <div className="flex justify-start pt-[16px] pl-[16px]">
+                <div className="pt-[16px] pl-[16px]">
                     <div className="bg-white/30 w-[151px] flex items-center h-[40px] px-2 gap-x-3 rounded-[100px] backdrop-blur-lg">
                         <img src={natashaavatar} className="rounded-full" />
                         <p className="text-[#0F1017] text-[12px]">
@@ -14,7 +14,7 @@ export default function NatashaSunny() {
                     </div>
                 </div>
 
-                <div className="flex justify-end pt-[24px] pr-[24px]">
+                <div className="pl-[284px] pt-[24px]">
                     <div className="bg-[#6673EA] w-[24px] h-[24px] rounded-full flex items-center justify-center">
                         <img src={IconPin} />
                     </div>
